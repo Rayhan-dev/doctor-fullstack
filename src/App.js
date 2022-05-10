f8ecf8fb67dc10e4830bdc84c3cf91a3396b0da8
Rayhan-dev <mohammodrayhan444@gmail.com>
2022-05-10 15:02:16 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Appointment from './Pages/Appointment/Appointment';
+import Contact from './Pages/Contact/Contact';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Register from './Pages/Register/Register';
+import Reviews from './Pages/Revies/Reviews';
 import Navbar from './Shared/Navbar';
 
 function App() {
@@ -7,6 +13,12 @@ function App() {
     <div >
       <Navbar></Navbar>
       <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/appointment' element={<Appointment></Appointment>}></Route>
+        <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+        <Route path='/contact' element={<Contact></Contact>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
       </Routes>
     </div>
   );
